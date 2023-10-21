61c18de25abfb291d39c4bc915c23557b35ee62d
Woohyun Choi <jy12.chung@gmail.com>
2023-10-21 17:49:33 +0800

    first commit

---
+++ DetailLink.js
@@ -0,0 +1,9 @@
+'use client'
+import { useRouter } from "next/navigation"
+
+export default function DetailLink(){
+    let router = useRouter()
+    return (
+        <button onClick={()=>{ router.back() }}>GO back</button>
+    )
+}
